ba8a95050be118c00c8b12d1e5cc684ed8b06418
Mert İshak Kılıç <mertishakilic@gmail.com>
2024-01-26 14:53:58 +0300

    IProductService

--- NLayerUdemyApp/NLayer.API/Controller/ProductsController.cs
+++ NLayerUdemyApp/NLayer.API/Controller/ProductsController.cs
@@ -10,20 +10,18 @@ namespace NLayer.API.Controller;
 public class ProductsController : CustomBaseController
 {
     private readonly IMapper _mapper;
-    private readonly IService<Product> _service;
-    private readonly IProductService _productService;
+    private readonly IProductService _service;
 
-    public ProductsController(IService<Product> service, IMapper mapper, IProductService productService)
+    public ProductsController(IMapper mapper, IProductService service)
     {
         _service = service;
         _mapper = mapper;
-        _productService = productService;
     }
 
     [HttpGet("[action]")]
     public async Task<IActionResult> GetProductsWithCategory()
     {
-        return CreateActionResult(await _productService.GetProductsWithCategory());
+        return CreateActionResult(await _service.GetProductsWithCategory());
     }
 
     [HttpGet]
