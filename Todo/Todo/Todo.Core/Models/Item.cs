0ce03134e1a04e3a0e93043bc1ff3711b9d6da57
Mert İshak Kılıç <mertishakilic@gmail.com>
2024-02-05 13:48:07 +0300

    init

---
+++ Todo/Todo/Todo.Core/Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace Todo.Todo.Core.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+}
