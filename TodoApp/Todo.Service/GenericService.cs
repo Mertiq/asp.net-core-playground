58445864a46369c079dc3a7a1236c492a1cef6c6
Mert İshak Kılıç <mertishakilic@gmail.com>
2024-02-08 11:26:30 +0300

    Service Layer

---
+++ TodoApp/Todo.Service/GenericService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Core.Repositories;
+using Todo.Core.Services;
+using Todo.Core.UnitOfWorks;
+
+namespace Todo.Service;
+
+public class GenericService<T> : IGenericService<T> where T : class
+{
+    private readonly IGenericRepository<T> _genericRepository;
+
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GenericService(IGenericRepository<T> genericRepository, IUnitOfWork unitOfWork)
+    {
+        _genericRepository = genericRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _genericRepository.GetByIdAsync(id);
+    }
+
+    public async Task<List<T>> GetAllAsync()
+    {
+        return await _genericRepository.GetAllAsync().ToListAsync();
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _genericRepository.AddAsync(entity);
+        await _unitOfWork.CommitAsync();
+        return entity;
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _genericRepository.UpdateAsync(entity);
+        await _unitOfWork.CommitAsync();
+    }
+
+    public Task DeleteAsync(T entity)
+    {
+        _genericRepository.DeleteAsync(entity);
+        return _unitOfWork.CommitAsync();
+    }
+}
